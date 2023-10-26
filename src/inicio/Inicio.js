5e571824281f22bf254f463f85d3878b538ca496
Amin Jalilie <jalilieportacio15@gmail.com>
2023-10-26 11:59:12 -0500

    actualizando

--- src/inicio/Inicio.js
+++ src/inicio/Inicio.js
@@ -2,7 +2,7 @@ import Navbar from '../navbar/Navbar';
 import Banner from '../banner/Banner';
 import Product from '../products/Product';
 
-
+//pagina de inicio
 const Inicio = () => {
    return(
        <>
